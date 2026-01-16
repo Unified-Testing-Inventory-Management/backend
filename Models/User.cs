e40f40fa2f4eabfb1383e4c73581e5bcde13fc18
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-16 11:52:42 +0800

    feat: add User model with properties for user management and association with products

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Server.Models;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; } // Owner
+    public DateOnly CreatedAt { get; set; }
+
+    public ICollection<Product> Products {get;set;} = new List<Product>();
+}
