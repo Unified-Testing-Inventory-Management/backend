0f15918dce51b28c8d89c650be7c4b2504ee13ec
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-16 11:52:27 +0800

    feat: add Sale_Detail model for sales transactions with product and quantity properties

---
+++ Models/Sale_Detail.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Server.Models;
+
+public class Sale_Detail
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid SaleId { get; set; }
+    [System.Text.Json.Serialization.JsonIgnore]
+    public Sale? Sale;
+    public Guid ProductId { get; set; }
+    [System.Text.Json.Serialization.JsonIgnore]
+    public Product? Product;
+    public int Quantity { get; set; }
+    public float Price { get; set; }
+}
