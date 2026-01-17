ef602d2d5d521fd67d5902f03692f78b2f3130bd
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-17 11:02:30 +0800

    refactor: modify LoginUser method in IUserInterface to remove return type for improved clarity

--- Interface/IUserInterface.cs
+++ Interface/IUserInterface.cs
@@ -1,12 +1,13 @@
 using System;
 
 namespace Server.Interface;
+
 using Server.DTOs;
 using Server.Models;
 
 public interface IUserInterface
 {
     Task RegisterUser(UserDTOs.RegisterUserDTOs _registerUserDTOs);
-    Task<string> LoginUser(UserDTOs.LoginUserDTOs _loginUserDTOs);
+    Task LoginUser(UserDTOs.LoginUserDTOs _loginUserDTOs);
     Task<User?> GetUserById(string? userId);
 }
