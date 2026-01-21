571838e7bc3f63ebaa43e5010e39a1b7386dd949
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-21 11:09:33 +0800

    feat: add IProductInterface for product management operations including CRUD and archiving

---
+++ Interface/IProductInterface.cs
@@ -0,0 +1,14 @@
+using System;
+using Server.DTOs;
+using Server.Models;
+
+namespace Server.Interface;
+
+public interface IProductInterface
+{
+    Task<List<Product>> GetAllProducts();
+    Task CreateProduct(ProductDTOs.CreateProductDTOs _createProductDTOs);
+    Task<Product> GetProductById(Guid id);
+    Task UpdateProductById(ProductDTOs.UpdateProductDTOs _upddateProductDTOs, Guid id);
+    Task ArchiveProductById(Guid id);
+}
