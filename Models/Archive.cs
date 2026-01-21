ddfa326afc36bbbbd1ce78c6389c9041503ab603
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-21 11:09:59 +0800

    feat: add Archive model to represent archived products with relevant properties

---
+++ Models/Archive.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Server.Models;
+
+public class Archive
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid? ProductId { get; set; }
+    [System.Text.Json.Serialization.JsonIgnore]
+    public Product? Product;
+    public string? ProductName { get; set; }
+    public string? Category { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public string? BarCode { get; set; }
+    public DateOnly CreatedAt { get; set; }
+
+    public Guid? UserId { get; set; }
+    [System.Text.Json.Serialization.JsonIgnore]
+    public User? User { get; set; }
+}
