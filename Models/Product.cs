9283abbd4d6e88af025e01d71935274688417282
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-21 11:10:10 +0800

    feat: enhance Product model with additional properties for image, barcode, and user association

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,11 +5,16 @@ namespace Server.Models;
 public class Product
 {
     public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Image { get; set; }
     public string? ProductName { get; set; }
     public string? Category { get; set; }
-    public float Price { get; set; }
+    public decimal Price { get; set; }
     public int StockQuantity { get; set; }
-    public int LowStockLevel { get; set; }
+    public string? BarCode { get; set; }
     public DateOnly CreatedAt { get; set; }
+
+    public Guid? UserId { get; set; }
+    [System.Text.Json.Serialization.JsonIgnore]
+    public User? User { get; set; }
 
 }
