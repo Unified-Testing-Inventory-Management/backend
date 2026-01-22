078f633f38b3335f7f6b5bef5284911c38aca3c6
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-22 14:35:13 +0800

    feat: implement TransactionController with endpoints for creating and retrieving product sales, secured with authorization

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Server.DTOs;
+using Server.Interface;
+
+namespace Server.Controllers
+{
+    [Route("api/v1/transactions")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+    private readonly ITransactionInterface _transactionServices;
+    
+    public TransactionController(ITransactionInterface transactionInterface)
+    {
+        _transactionServices = transactionInterface;
+    }
+    
+    [Authorize(Policy = "OwnerOnly")]
+    [HttpPost]
+    public async Task<IActionResult> CreateSale(TransactionDTOs.CreateProductSale _createSale)
+    {
+        try
+        {
+            await _transactionServices.CreateProductSale(_createSale);
+            return Ok(new {message = "Transaction Successfully"});
+        }
+        catch (Exception e)
+        {
+            return NotFound( new { error = e.Message } );
+        }
+    }
+
+    [Authorize(Policy = "OwnerOnly")]
+    [HttpGet]
+    public async Task<IActionResult> GetAllProductSale()
+    {
+        try
+        {
+            var saleProduct = await _transactionServices.AllProductTransaction();
+            return Ok(new {message = "Transaction Successfully",data = saleProduct});
+        }
+        catch (Exception e)
+        {
+            return NotFound(new {error = e.Message});
+        }
+    }
+    }
+}
