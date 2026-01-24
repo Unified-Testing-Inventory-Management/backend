7ea9c23713060078276e1fa2a88e5f79f03076fa
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-24 19:23:40 +0800

    fix: change CreatedAt property type from DateOnly to DateTime in Product model for consistency

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,7 +11,7 @@ public class Product
     public decimal Price { get; set; }
     public int StockQuantity { get; set; }
     public string? BarCode { get; set; }
-    public DateOnly CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
 
     public Guid? UserId { get; set; }
     [System.Text.Json.Serialization.JsonIgnore]
