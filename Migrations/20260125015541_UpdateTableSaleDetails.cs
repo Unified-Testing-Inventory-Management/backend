bd623f076448e0337f47c334ddc2e689bbc010ab
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-25 12:08:42 +0800

    feat: add Category column to Sale_Details table for enhanced product categorization

---
+++ Migrations/20260125015541_UpdateTableSaleDetails.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableSaleDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Sale_Details",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Sale_Details");
+        }
+    }
+}
