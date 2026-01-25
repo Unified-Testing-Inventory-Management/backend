52479ca21366237dd57d6d49cadbde49e6bd1b02
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-25 12:08:26 +0800

    feat: add Category property to TransactionDTOs for improved product categorization

--- DTOs/TransactionDTOs.cs
+++ DTOs/TransactionDTOs.cs
@@ -8,6 +8,7 @@ public class TransactionDTOs
     {
     public Guid ProductId { get; set; }
     public required string ProductName { get; set; }
+    public required string Category { get; set; }
     public Guid? UserId { get; set; }
     public int TotalAmount { get; set; }
     public int Quantity { get; set; }
