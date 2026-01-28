05efefdd5946adddbd9e9c43dc1cf2198b30fab8
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-28 11:51:24 +0800

    feat: add SearchProduct method to IProductInterface for product name search functionality

--- Interface/IProductInterface.cs
+++ Interface/IProductInterface.cs
@@ -9,6 +9,7 @@ public interface IProductInterface
     Task<List<Product>> GetAllProducts();
     Task CreateProduct(ProductDTOs.CreateProductDTOs _createProductDTOs);
     Task<Product> GetProductById(Guid id);
+    Task<List<Product>> SearchProduct(string productName);
     Task UpdateProductById(ProductDTOs.UpdateProductDTOs _upddateProductDTOs, Guid id);
     Task ArchiveProductById(Guid id);
 }
