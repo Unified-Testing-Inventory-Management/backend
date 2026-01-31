8875272c79b5fe72156f7a94bd1a2aa486f5f5ce
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-31 17:32:27 +0800

    refactor: update UserDTOs role from Owner to Admin for consistency with authorization policies

--- DTOs/UserDTOs.cs
+++ DTOs/UserDTOs.cs
@@ -10,7 +10,7 @@ public class UserDTOs
         public required string LastName { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; }
-        public required string Role { get; set; } // Owner
+        public required string Role { get; set; } // Admin
     }
 
     public class LoginUserDTOs
