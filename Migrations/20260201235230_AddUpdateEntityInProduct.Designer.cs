4847fede365af370a199c3342d21109003af82a8
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-02 21:53:24 +0800

    feat: add CreatedAt and UpdatedAt columns to Archives and Products for better tracking of entity modifications

--- Migrations/20260201235230_AddUpdateEntityInProduct.Designer.cs
+++ Migrations/20260201235230_AddUpdateEntityInProduct.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.Data;
 
@@ -11,9 +12,11 @@ using Server.Data;
 namespace Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260201235230_AddUpdateEntityInProduct")]
+    partial class AddUpdateEntityInProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -34,9 +37,6 @@ namespace Server.Migrations
                     b.Property<string>("Category")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<DateTime>("CreatedAt")
-                        .HasColumnType("datetime2");
-
                     b.Property<DateTime>("DeletedAt")
                         .HasColumnType("datetime2");
 
@@ -51,9 +51,6 @@ namespace Server.Migrations
 
                     b.Property<int>("StockQuantity")
                         .HasColumnType("int");
-
-                    b.Property<DateTime>("UpdatedAt")
-                        .HasColumnType("datetime2");
 
                     b.Property<Guid?>("UserId")
                         .HasColumnType("uniqueidentifier");
