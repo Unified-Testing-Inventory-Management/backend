1aa95c6cc85527f188ad6deb2328157b3d8423c3
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-06 15:25:38 +0800

    feat: introduce IProductService interface to define product management operations

---
+++ Interface/IProductService.cs
@@ -0,0 +1,19 @@
+using System;
+using Server.DTOs;
+using Server.Models;
+
+namespace Server.Interface;
+
+public interface IProductService
+{
+    Task<List<Product>> GetAllProducts();
+    Task CreateProduct(ProductDTOs.CreateProductDTOs _createProductDTOs);
+    Task<Product?> GetProductById(Guid id);
+    Task<List<Product>> SearchProduct(string productName);
+    Task UpdateProductById(ProductDTOs.UpdateProductDTOs _upddateProductDTOs, Guid id);
+    Task ArchiveProductById(Guid id);
+    Task<List<Archive>> SearchArchiveProduct(string productName);
+    Task<List<Archive>>  GetAllArchiveProducts();
+    Task RestoreArchiveInProduct(Guid id);
+    Task DeleteProductInArchive(Guid id);
+}
