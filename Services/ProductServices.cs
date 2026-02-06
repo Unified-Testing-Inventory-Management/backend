80ae80249536226a7dc0eb5eb9b572225d573ed2
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-06 15:27:29 +0800

    refactor: update ProductServices to use IProductRepository for data access and improve service injection

--- Services/ProductServices.cs
+++ Services/ProductServices.cs
@@ -1,5 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Server.Data;
 using Server.DTOs;
 using Server.Exceptions;
 using Server.Interface;
@@ -7,24 +5,16 @@ using Server.Models;
 
 namespace Server.Services
 {
-    public class ProductServices : IProductInterface
+    public class ProductServices(CurrentUserServices currentUserServices, BarCodeServices barCodeServices, IProductRepository productRepositories) : IProductService
     {
-        private readonly AppDbContext _db;
-        private CurrentUserServices _currentUserServices;
-        private BarCodeServices _barCodeServices;
-
-        public ProductServices(AppDbContext db, CurrentUserServices currentUserServices, BarCodeServices barCodeServices)
-        {
-            _db = db;
-            _currentUserServices = currentUserServices;
-            _barCodeServices = barCodeServices;
-        }
-
+        private readonly CurrentUserServices _currentUserServices = currentUserServices;
+        private readonly BarCodeServices _barCodeServices = barCodeServices;
+        private readonly IProductRepository _productRepositories = productRepositories;
 
         public async Task<List<Product>> GetAllProducts()
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            return await _db.Products.Where(p => p.UserId == userId).OrderByDescending(p => p.CreatedAt).ToListAsync();
+            return await _productRepositories.GetAllProducts(userId);
         }
 
         public async Task CreateProduct(ProductDTOs.CreateProductDTOs _createProductDtOs)
@@ -40,11 +30,10 @@ namespace Server.Services
             if (_createProductDtOs.StockQuantity < 0)
                 throw new ProductExceptions.InvalidStockQuantityException();
 
-            var productInArchive = await _db.Archives.FirstOrDefaultAsync(p => p.ProductName == _createProductDtOs.ProductName && p.UserId == userId);
-            var productExists = await _db.Products.FirstOrDefaultAsync(p =>
-                p.ProductName == _createProductDtOs.ProductName &&
-                p.UserId == userId
-            );
+            var productInArchive = await _productRepositories.GetProductNameInArchive(_createProductDtOs.ProductName, userId);
+
+            var productExists = await _productRepositories.GetProductNameInProduct(_createProductDtOs.ProductName, userId);
+
 
             if (productExists != null)
                 throw new ProductExceptions.ProductAlreadyExists(productExists.ProductName!);
@@ -98,153 +87,84 @@ namespace Server.Services
 
             product.BarCode = _barCodeServices.GenerateProductBarcode(productId);
 
-            _db.Products.Add(product);
-            await _db.SaveChangesAsync();
+            await _productRepositories.SaveProduct(product);
         }
 
-        public async Task<Product> GetProductById(Guid id)
+        public async Task<Product?> GetProductById(Guid id)
         {
             var userId = _currentUserServices.GetLoggedInUser();
 
-            var product = await _db.Products
-                .FirstOrDefaultAsync(p => p.Id == id && p.UserId == userId);
+            var product = _productRepositories.GetProductById(userId, id);
 
             if (product == null)
-            {
                 throw new ProductExceptions.ProductNotFoundException(id);
-            }
 
-            return product;
+            return await product;
         }
 
         public async Task<List<Product>> SearchProduct(string productName)
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            var query = _db.Products.Where(u => u.UserId == userId).AsQueryable();
-
-            if (!string.IsNullOrEmpty(productName))
-            {
-                query = query.Where(p => EF.Functions.Like(p.ProductName, $"%{productName}%"));
-            }
-
-            return await query.ToListAsync();
+            return await _productRepositories.SearchNameInProduct(userId, productName);
         }
 
         public async Task ArchiveProductById(Guid id)
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            var product = await _db.Products.FirstOrDefaultAsync(p => p.UserId == userId && p.Id == id);
+            var product = await _productRepositories.GetProductById(userId, id);
 
             if (product == null)
-            {
                 throw new ProductExceptions.ProductNotFoundException(id);
-            }
 
-            _db.Products.Remove(product);
-
-            var Id = Guid.NewGuid();
-            var productSaveInArchive = new Archive
-            {
-                Id = Id,
-                ProductId = product.Id,
-                UserId = userId,
-                ProductName = product.ProductName,
-                Category = product.Category,
-                Price = product.Price,
-                StockQuantity = product.StockQuantity,
-                BarCode = product.BarCode,
-                CreatedAt = product.CreatedAt,
-                UpdatedAt = product.UpdatedAt,
-                DeletedAt = DateTime.Now
-            };
-
-            _db.Archives.Add(productSaveInArchive);
-            await _db.SaveChangesAsync();
+            await _productRepositories.SaveProductInArchive(userId, product);
         }
 
-        public Task<List<Archive>> SearchArchiveProduct(string productName)
+        public async Task<List<Archive>> SearchArchiveProduct(string productName)
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            var query = _db.Archives.Where(ap => ap.UserId == userId).AsQueryable();
-
-            if (!string.IsNullOrEmpty(productName))
-            {
-                query = query.Where(ap => EF.Functions.Like(ap.ProductName, $"%{productName}%"));
-            }
-
-            return query.ToListAsync();
+            return await _productRepositories.SearchNameInArchive(userId, productName);
         }
 
         public async Task UpdateProductById(ProductDTOs.UpdateProductDTOs _updateProductDTOs, Guid id)
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            var product = await _db.Products.FirstOrDefaultAsync(p => p.UserId == userId && p.Id == id);
+            var product = await _productRepositories.GetProductById(userId, id);
 
             if (product == null)
-            {
                 throw new ProductExceptions.ProductNotFoundException(id);
-            }
 
             if (_updateProductDTOs.Price < 0)
-            {
                 throw new ProductExceptions.InvalidProductPriceException();
-            }
 
-            product.ProductName = _updateProductDTOs.ProductName;
-            product.Category = _updateProductDTOs.Category;
-            product.Price = _updateProductDTOs.Price;
-            product.StockQuantity = _updateProductDTOs.StockQuantity;
-            product.UpdatedAt = DateTime.Now;
-
-            await _db.SaveChangesAsync();
-
+            await _productRepositories.UpdateProduct(_updateProductDTOs, product);
         }
 
-        public async Task<List<Archive>> GetAllArchivesProduct()
+        public async Task<List<Archive>> GetAllArchiveProducts()
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            var productArchives = await _db.Archives.Where(a => a.UserId == userId).OrderByDescending(a => a.DeletedAt).ToListAsync();
-            return productArchives;
+            return await _productRepositories.GetAllArchiveProducts(userId);
         }
 
-        public async Task RestoreArchiveProduct(Guid id)
+        public async Task RestoreArchiveInProduct(Guid id)
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            var productArchive = await _db.Archives.FirstOrDefaultAsync(a => a.UserId == userId && a.ProductId == id);
+            var productArchive = await _productRepositories.GetProductArchiveById(userId, id);
+
             if (productArchive == null)
-            {
                 throw new ProductExceptions.ProductNotFoundException(id);
-            }
-            _db.Archives.Remove(productArchive);
 
-            var restoreProduct = new Product
-            {
-                Id = productArchive.ProductId,
-                UserId = productArchive.UserId,
-                ProductName = productArchive.ProductName,
-                Category = productArchive.Category,
-                Price = productArchive.Price,
-                StockQuantity = productArchive.StockQuantity,
-                BarCode = productArchive.BarCode,
-                CreatedAt = productArchive.CreatedAt,
-                UpdatedAt = productArchive.UpdatedAt
-            };
-
-            _db.Products.Add(restoreProduct);
-            await _db.SaveChangesAsync();
+            await _productRepositories.RestoreProductInArchive(userId, productArchive);
         }
 
-        public async Task DeleteArchiveProduct(Guid id)
+        public async Task DeleteProductInArchive(Guid id)
         {
             var userId = _currentUserServices.GetLoggedInUser();
-            var productArchive = await _db.Archives.FirstOrDefaultAsync(a => a.UserId == userId && a.ProductId == id);
-            if (productArchive == null)
-            {
-                throw new ProductExceptions.ProductNotFoundException(id);
-            }
-            _db.Archives.Remove(productArchive);
+            var productArchive = await _productRepositories.GetProductArchiveById(userId, id);
 
-            await _db.SaveChangesAsync();
+            if (productArchive == null)
+                throw new ProductExceptions.ProductNotFoundException(id);
+
+            await _productRepositories.DeleteProductInArchive(productArchive);
         }
     }
 }
