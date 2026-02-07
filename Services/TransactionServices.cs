8c44008d5a8830dfbe628a61ca2ae8dd2efc846c
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-07 15:34:57 +0800

    refactor: update TransactionServices to use ITransactionRepository for data access and improve constructor injection

--- Services/TransactionServices.cs
+++ Services/TransactionServices.cs
@@ -8,25 +8,15 @@ using Server.Models;
 
 namespace Server.Services;
 
-public class TransactionServices : ITransactionService
+public class TransactionServices(ITransactionRepository transactionRepository, CurrentUserServices currentUserServices) : ITransactionService
 {
-    private readonly AppDbContext _db;
-    private readonly CurrentUserServices _currentUserServices;
-    
-    public TransactionServices(AppDbContext appDb, CurrentUserServices currentUserServices)
-    {
-        _db = appDb;
-        _currentUserServices = currentUserServices;
-    }
-    
+    private readonly CurrentUserServices _currentUserServices = currentUserServices;
+    private readonly ITransactionRepository _transactionRepository = transactionRepository;
     public async Task CreateProductSale(TransactionDTOs.CreateProductSale _createSale)
     {
         var userId = _currentUserServices.GetLoggedInUser();
-        var productTransaction = await _db.Products.FirstOrDefaultAsync(p => p.UserId == userId && p.Id == _createSale.ProductId);
+        var productTransaction = await _transactionRepository.GetProductById(userId, _createSale.ProductId) ?? throw new TransactionExceptions.ProductInTrasactionNotFoundException(_createSale.ProductName);
 
-        if (productTransaction == null)
-            throw new TransactionExceptions.ProductInTrasactionNotFoundException(_createSale.ProductName);
-        
         if (productTransaction.StockQuantity >= _createSale.Quantity)
         {
             productTransaction.StockQuantity -= _createSale.Quantity;
@@ -38,53 +28,24 @@ public class TransactionServices : ITransactionService
 
         var saleId = Guid.NewGuid();
         var saleDetailId = Guid.NewGuid();
-        var totalAmount = (_createSale.Quantity * _createSale.Price);
-        
-        var saveProductSale = new Sale
-        {
-            Id = saleId,
-            ProductId =_createSale.ProductId,
-            UserId = userId,
-            SaleDate = DateTime.Now,
-            TotalAmount = totalAmount
-        };
+        var totalAmount = _createSale.Quantity * _createSale.Price;
 
-        var saleProductDetails = new Sale_Detail
-        {
-            Id = saleDetailId,
-            ProductName = _createSale.ProductName,
-            Category = _createSale.Category,
-            ProductId = saveProductSale.ProductId,
-            SaleId = saveProductSale.Id,
-            Price = _createSale.Price,
-            Quantity = _createSale.Quantity,
-        };
-        
-        _db.Sales.Add(saveProductSale);
-        _db.Sale_Details.Add(saleProductDetails);
-        
-        await _db.SaveChangesAsync();
+        var saveProductSale = await _transactionRepository.SaveProductInSale(userId, saleId, totalAmount, _createSale.ProductId);
+
+        await _transactionRepository.SaveProductInSaleDetails(saleDetailId, saveProductSale.Id, saveProductSale.ProductId, _createSale);
+
     }
 
     public async Task<List<Sale>> AllProductTransaction()
     {
-        var  userId = _currentUserServices.GetLoggedInUser();
-        return await _db.Sales
-            .Include(s => s.SaleDetails).Where(s => s.UserId == userId)
-            .OrderByDescending(s => s.SaleDate)
-            .ToListAsync();
+        var userId = _currentUserServices.GetLoggedInUser();
+        return await _transactionRepository.AllProductTransactions(userId);
     }
 
     public async Task<List<Sale>> SearchSaleProductTransaction(string productName)
     {
         var userId = _currentUserServices.GetLoggedInUser();
-        var query = _db.Sales.Where(sp => sp.UserId == userId).AsQueryable();
-        
-        if (!string.IsNullOrEmpty(productName))
-        {
-           query = query.Where(p  => p.SaleDetails.Any(s => s.ProductName == productName));
-        }
-        
-        return await query.ToListAsync();
+        var query = _transactionRepository.SearchSaleProductTransaction(userId, productName);
+        return await query;
     }
 }
