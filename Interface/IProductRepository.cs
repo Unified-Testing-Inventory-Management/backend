dc5077dc66860b83681fef8ba69a2354ed763553
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-07 19:38:05 +0800

    feat: update IProductRepository to modify SaveProduct method signature and add SaveProductFromExcelImport for enhanced product management

--- Interface/IProductRepository.cs
+++ Interface/IProductRepository.cs
@@ -13,10 +13,11 @@ public interface IProductRepository
     Task<List<Archive>> SearchNameInArchive(Guid userId, string productName);
     Task<Product?> GetProductNameInProduct(string productName, Guid userId);
     Task<Archive?> GetProductNameInArchive(string productName, Guid userId);
-    Task SaveProduct(Product product);
+    Task SaveProduct(Guid userId, string imagePath, ProductDTOs.CreateProductDTOs _createProductDTOs);
     Task UpdateProduct(ProductDTOs.UpdateProductDTOs updateProductDTOs, Product product);
     Task SaveProductInArchive(Guid userId, Product product);
     Task<List<Archive>> GetAllArchiveProducts(Guid userId);
     Task RestoreProductInArchive(Guid userId, Archive archive);
     Task DeleteProductInArchive(Archive archive);
+    Task SaveProductFromExcelImport(Guid userId,string productName,string category,int price,int stockQuantity);
 }
