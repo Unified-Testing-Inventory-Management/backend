908a8fc82f1f7b00db7d4ffc850f5abb06f75a8b
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-08 08:07:51 +0800

    refactor: update ITransactionRepository interface to simplify SaveProductInSale method signature and adjust parameters for SaveProductInSaleDetails

--- Interface/ITransactionRepository.cs
+++ Interface/ITransactionRepository.cs
@@ -7,8 +7,8 @@ namespace Server.Interface;
 public interface ITransactionRepository
 {
     Task<Product?> GetProductById(Guid userId, Guid id);
-    Task<Sale> SaveProductInSale(Guid userId, Guid saleId, int totalAmount, Guid productId);
-    Task SaveProductInSaleDetails(Guid saleDetailId, Guid saleId, Guid productId, TransactionDTOs.CreateProductSale createProductSale);
+    Task<Sale> SaveProductInSale(Guid userId, int totalAmount, Guid productId);
+    Task SaveProductInSaleDetails(Guid saleId, Guid productId, TransactionDTOs.CreateProductSale createProductSale);
     Task<List<Sale>> AllProductTransactions(Guid userId);
     Task<List<Sale>> SearchSaleProductTransaction(Guid userId,string productName);
 }
