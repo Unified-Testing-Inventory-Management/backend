4a1964c40fa3d397ef18160a9561c644b4c2fd71
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-10 12:01:13 +0800

    refactor: modify TransactionExceptions to include message and status code parameters for enhanced error handling

--- Exceptions/TransactionExceptions.cs
+++ Exceptions/TransactionExceptions.cs
@@ -2,23 +2,19 @@ namespace Server.Exceptions;
 
 public class TransactionExceptions
 {
-    public class ProductInTrasactionNotFoundException : Exception
+    public class ProductInTransactionNotFoundException(string message, int status) : Exception
     {
-        public string ProductName { get; }
-
-        public ProductInTrasactionNotFoundException(string productName): base($"Product {productName} in transaction not found")
-        {
-            ProductName = productName;
-        }
+        override
+        public string Message
+        { get; } = message;
+        public int StatusCode { get; set; } = status;
     }
 
-    public class NotEnoughStockException : Exception
+    public class NotEnoughStockException(string message, int status) : Exception
     {
-        private string ProductName { get; }
-
-        public NotEnoughStockException(string productName) : base($"Not enough stock in transaction {productName}")
-        {
-            ProductName = productName;
-        }
+        override
+        public string Message
+        { get; } = message;
+        public int StatusCode { get; set; } = status;
     }
 }
