b92c7ac56a4df9011b2091759eeeaa4e29badbd7
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-05-19 12:53:49 +0800

    refactor: change SaveUser method to return User object instead of void for improved usability

--- Interface/IUserRepository.cs
+++ Interface/IUserRepository.cs
@@ -6,7 +6,7 @@ namespace Server.Interface;
 public interface IUserRepository
 {
     Task<User?> GetUser(string username);
-    Task SaveUser(UserDTOs.RegisterUserDTOs registerUserDTOs);
+    Task<User> SaveUser(UserDTOs.RegisterUserDTOs registerUserDTOs);
     Task<User?> GetUserId(string? userId);
     Task<User?> FindUserById(Guid userId);
     Task UpdateUser(User user, UserDTOs.UpdateUserDTOs _updateUserDTOs);
